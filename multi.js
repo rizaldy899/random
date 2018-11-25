5960b4e1074706e5932eb1b7a42480f89d507d09
rizaldy899 <45313965+rizaldy899@users.noreply.github.com>
2018-11-26 07:29:27 +0800

    Update multi.js

--- multi.js
+++ multi.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="/2018/11/best-cheap-vacation-packages-6-great.html"}
-if(x=="2"){xxx="/2018/11/laughlin-hotel-deals-guide-which-hotel.html"}
+if(x=="1"){xxx="/2018/11/8-tips-for-when-youve-not-socialised-in.html"}
+if(x=="2"){xxx="/2018/11/5-useful-tips-for-camping-in-rain.html"}
 if(x=="3"){xxx="/2018/11/how-to-keep-your-gadgets-safe-while-on.html"}
-if(x=="4"){xxx="/2018/11/5-useful-tips-for-camping-in-rain.html"}
-if(x=="5"){xxx="/2018/11/8-tips-for-when-youve-not-socialised-in.html"}a_to_ve[i].href="https://kakitenet.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="/2018/11/laughlin-hotel-deals-guide-which-hotel.html"}
+if(x=="5"){xxx="/2018/11/best-cheap-vacation-packages-6-great.html"}a_to_ve[i].href="https://kakitenet.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
